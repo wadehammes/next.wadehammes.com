6590eb692944bd72cb3a5661c0548828c78145e3
Wade Hammes <wade.hammes@gmail.com>
2021-03-17 17:12:48 -0400

    remove gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,10 +45,6 @@ const Footer = styled.footer`
   width: 100%;
   z-index: 1;
   padding: ${({ theme }) => theme.sizing.mobilePadding};
-  background-image: linear-gradient(
-    transparent,
-    ${({ theme }) => theme.colors.black}
-  );
 
   @media ${device.tablet} {
     padding: ${({ theme }) => theme.sizing.desktopPadding};
