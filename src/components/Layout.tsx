70da1492aa10a3bb8b83300d5ed9244adf89d35f
Wade Hammes <wade.hammes@gmail.com>
2021-03-18 10:32:15 -0400

    more grid updates

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-rows: 1fr 4fr;
-  grid-gap: 5em;
+  grid-template-rows: 12em 1fr;
+  grid-gap: 0;
   height: 100vh;
   width: 100%;
 `;
