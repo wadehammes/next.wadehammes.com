40d5e343d255140551f15f07501d7f1e15d41e0a
Wade Hammes <wade.hammes@gmail.com>
2021-03-18 12:36:16 -0400

    add circle ci

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_API_TOKEN: process.env.CONTENTFUL_API_TOKEN,
+    ENVIRONMENT: process.env.ENVIRONMENT,
   },
   images: {
     domains: ["images.ctfassets.net"],
