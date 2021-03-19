bd0fb0d9f54bec0c852e016fb9a9cb5efbde5698
Wade Hammes <wadehammes@users.noreply.github.com>
2021-03-19 15:24:06 -0400

    💄 fix minor font sizing issues (#32)

--- src/components/Button/ButtonGroup.component.tsx
+++ src/components/Button/ButtonGroup.component.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import { device } from "src/styles/theme";
 import styled from "styled-components";
 
 const Group = styled.div`
@@ -7,11 +6,7 @@ const Group = styled.div`
   align-items: center;
   justify-content: flex-start;
   padding-top: 0.5rem;
-  font-size: 0.9rem;
-
-  @media ${device.tablet} {
-    font-size: 1rem;
-  }
+  font-size: 1rem;
 
   > * + * {
     margin-left: 0.75rem;
