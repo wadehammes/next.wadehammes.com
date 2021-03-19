49f5c9c495a154eed1a5d187553711c4e2ef6fcb
Wade Hammes <wade.hammes@gmail.com>
2021-03-19 16:20:30 -0400

    fix typings for page

--- src/components/Page/Page.component.tsx
+++ src/components/Page/Page.component.tsx
@@ -1,19 +1,25 @@
-import { forwardRef, Ref } from "react";
+import { forwardRef, ReactElement, Ref } from "react";
 import { Header } from "src/components/Header/Header.component";
 import { Content, Grid } from "src/components/Layout";
 import { Helmet } from "src/components/Page/Helmet.component";
 import { useWindowDimensions } from "src/hooks/useWindowDimensions";
 
-export const Page = forwardRef(({ children }, ref: Ref<HTMLDivElement>) => {
-  const { height } = useWindowDimensions();
+interface PageProps {
+  children?: ReactElement | ReactElement[];
+}
 
-  return height ? (
-    <>
-      <Helmet />
-      <Grid ref={ref} gridHeight={height}>
-        <Header />
-        <Content>{children}</Content>
-      </Grid>
-    </>
-  ) : null;
-});
+export const Page = forwardRef(
+  ({ children }: PageProps, ref: Ref<HTMLDivElement>) => {
+    const { height } = useWindowDimensions();
+
+    return height ? (
+      <>
+        <Helmet />
+        <Grid ref={ref} gridHeight={height}>
+          <Header />
+          <Content>{children}</Content>
+        </Grid>
+      </>
+    ) : null;
+  },
+);
