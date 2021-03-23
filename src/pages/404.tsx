234127567f0c489269ebab880dc69e89d536b9f0
Wade Hammes <wadehammes@users.noreply.github.com>
2021-03-23 15:11:11 -0400

    🧹 remove animate wrapper until more performant (#37)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { SpiralsAbout } from "src/components/Spirals/SpiralsAbout.component";
 import { SpiralsSVG } from "src/components/Spirals/SpiralsSVG.component";
 import parse from "html-react-parser";
-import { Animate } from "src/components/Animate/Animate.component";
 import { useInView } from "react-intersection-observer";
 
 const E404: FC = () => {
@@ -18,18 +17,16 @@ const E404: FC = () => {
     <>
       <Page ref={ref}>
         <Footer>
-          <Animate visible={inView}>
-            <H1>Error: 404</H1>
-            <P>
-              Whoops, {parse("you&apos;ve")} landed on a page that{" "}
-              {parse("doesn&apos;t")} exist. No worries. Just head{" "}
-              <Link href="/">
-                <A>home</A>
-              </Link>
-              .
-            </P>
-            <SpiralsAbout />
-          </Animate>
+          <H1>Error: 404</H1>
+          <P>
+            Whoops, {parse("you&apos;ve")} landed on a page that{" "}
+            {parse("doesn&apos;t")} exist. No worries. Just head{" "}
+            <Link href="/">
+              <A>home</A>
+            </Link>
+            .
+          </P>
+          <SpiralsAbout />
         </Footer>
       </Page>
       <SpiralsSVG visible={inView} />
