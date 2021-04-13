c74b5d59784d4601ddc3d46a33f4f3ee2232b787
Wade Hammes <wadehammes@users.noreply.github.com>
2021-04-13 07:11:30 -0400

    ⚙ add security headers (#50)

--- next.config.js
+++ next.config.js
@@ -11,4 +11,91 @@ module.exports = {
     webpack5: true,
     strictPostcssConfiguration: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate",
+          },
+          ...securityHeaders,
+        ],
+      },
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate",
+          },
+          ...securityHeaders,
+        ],
+      },
+    ];
+  },
 };
+
+// https://securityheaders.com
+const scriptSrc = [
+  "'self'",
+  "'unsafe-eval'",
+  "'unsafe-inline'",
+  "*.youtube.com",
+  "*.ctfassets.net",
+  "*.google-analytics.com",
+  "*.vercel-insights.com",
+  "*.vercel.app",
+];
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src ${scriptSrc.join(" ")};
+  child-src *.youtube.com *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data: *.ctfassets.net;
+  media-src 'none';
+  connect-src *;
+  font-src 'self' fonts.gstatic.com;
+  worker-src 'self' *.vercel.app;
+  manifest-src 'self' *.vercel.app;
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
