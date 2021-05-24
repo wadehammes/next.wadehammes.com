892fb0b27f4c510f19a97f96ca693163b4bb3c6b
Wade Hammes <wade.hammes@gmail.com>
2021-05-24 09:34:13 -0400

    clean up envvars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   env: {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_API_TOKEN: process.env.CONTENTFUL_API_TOKEN,
     ENVIRONMENT: process.env.ENVIRONMENT,
   },
   images: {
