9be03576eb9c9cd13f771f749a4dcaa93b775417
Wade Hammes <wadehammes@users.noreply.github.com>
2024-01-16 23:38:05 -0500

    package updates 011624 (#820)

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -4,9 +4,6 @@ import { Crown } from "src/styles/icons/crown";
 import Link from "next/link";
 import { device } from "src/styles/theme";
 import { Container } from "src/components/Layout";
-import { usePreferredTheme, Themes } from "src/hooks/usePreferredTheme";
-import { Sun } from "src/styles/icons/sun";
-import { Moon } from "src/styles/icons/moon";
 
 const HeaderContainer = styled(Container)`
   display: flex;
@@ -16,37 +13,6 @@ const HeaderContainer = styled(Container)`
   z-index: 99;
   padding-top: 0;
   padding-bottom: 0;
-`;
-
-const ThemeToggleContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  flex: 1;
-  transition: transform 0.2s ease-in;
-  transform-origin: center center;
-`;
-
-const ThemeToggle = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: transparent;
-  border: 0;
-  padding: 0;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:hover:active {
-    transform: translateY(2px);
-  }
-
-  > svg {
-    height: 24px;
-    width: 24px;
-  }
 `;
 
 const Logo = styled.div`
@@ -71,8 +37,6 @@ const Logo = styled.div`
 `;
 
 export const Header: FC = () => {
-  const { currentTheme, updateTheme } = usePreferredTheme();
-
   return (
     <HeaderContainer>
       <Logo>
@@ -80,18 +44,6 @@ export const Header: FC = () => {
           <Crown />
         </Link>
       </Logo>
-      <ThemeToggleContainer>
-        <ThemeToggle
-          type="button"
-          onClick={() =>
-            updateTheme(
-              currentTheme === Themes.Light ? Themes.Dark : Themes.Light,
-            )
-          }
-        >
-          {currentTheme === Themes.Light ? <Moon /> : <Sun />}
-        </ThemeToggle>
-      </ThemeToggleContainer>
     </HeaderContainer>
   );
 };
