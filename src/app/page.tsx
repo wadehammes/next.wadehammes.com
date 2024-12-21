d5a1deefd4d224c157dc51ece010d3ab33396da4
Wade Hammes <wade.hammes@gmail.com>
2024-12-21 13:29:06 -0500

    Chore: add title back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import { HomePage } from "src/components/HomePage/HomePage.component";
 
 export function generateMetadata(): Metadata {
   return {
+    title: "Wade Hammes - Software Engineer/Entrepreneur",
     metadataBase: new URL("https://wadehammes.com/"),
     creator: "Wade Hammes",
     publisher: "Wade Hammes",
     description:
-      "Wade Hammes is a software engineer for Rhythm Energy, helping build the best customer experience in retail renewable energy, and a co-founder of Provisioner, a full-service creative agency helping to grow brands.",
+      "Wade Hammes is a senior software engineer for Rhythm Energy, helping build the best customer experience in retail renewable energy, and a co-founder of Provisioner, a full-service creative agency helping to grow brands.",
   };
 }
 
